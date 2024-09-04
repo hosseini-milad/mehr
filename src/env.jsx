f191522c1e56c2dfb0938f7b851e799f3cbb24be
Amir Hossein Salimi <ah.salimi1431@gmail.com>
2024-09-04 16:47:18 +0330

    bug fixed

--- src/env.jsx
+++ src/env.jsx
@@ -1,11 +1,11 @@
 const env={
     //siteApi:'http://localhost:4000/api',
-    //siteApi:'https://panel.mehrgaz.com/api',
+    // siteApi:'https://panel.mehrgaz.com/api',
     //siteApi:'https://orderadmin.deleves.com/api',
     siteApi:'https://testpanel.mehrgaz.com/api',
     
     // siteApiUrl:'http://localhost:4000',
-    //siteApiUrl:'https://panel.mehrgaz.com',
+    // siteApiUrl:'https://panel.mehrgaz.com',
     //siteApiUrl:'https://orderadmin.deleves.com',
     siteApiUrl:'https://testpanel.mehrgaz.com',
 
